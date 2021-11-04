f0729bae5ce3a1c9527f0b9bc95fd3f939b5794b
BUdawgs <pstahldds@gmail.com>
2021-11-03 22:53:37 -0400

    complete qualified assignment

---
+++ public/src/books.js
@@ -0,0 +1,43 @@
+function findAuthorById(authors, id) {
+  let found = authors.find(author => author.id === id);
+  return found;
+}
+
+function findBookById(books, id) {
+  let found = books.find(book => book.id === id);
+  return found;
+}
+
+function partitionBooksByBorrowedStatus(books) {
+  let available = [];
+  let unavailable = [];
+  const bookStatuses = [];
+
+  books.forEach((book) => {
+    const isBookReturned = book.borrows[0].returned;
+  
+  if (isBookReturned) {
+    unavailable.push(book);
+    } else { 
+    available.push(book);
+    }
+   });
+    bookStatuses.push(available);
+    bookStatuses.push(unavailable);
+    return bookStatuses;
+  }
+
+
+function getBorrowersForBook(book, accounts) {
+  return book.borrows.map(borrow => {
+    let account = accounts.find(account => account.id === borrow.id);
+    return {...borrow, ...account};
+  }).slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
---
+++ public/src/home.js
@@ -0,0 +1,60 @@
+function getTotalBooksCount(books) {
+  return books.length;
+}
+
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
+
+function getBooksBorrowedCount(books) {
+  let booksUnreturned = books.filter(book => book.borrows.filter(record => record.returned === false).length > 0);
+  return booksUnreturned.length;
+  }
+
+
+function getMostCommonGenres(books) {
+  let map = {};
+  books.forEach(num => {
+    if (map[num.genre]) {
+      map[num.genre]++;
+    } else {
+      map[num.genre] = 1;
+    }
+  });
+  return Object.entries(map).map(([name, count]) => {
+    return {
+      name,
+      count
+    }
+  }).sort((alpha, beta) => beta.count - alpha.count).slice(0, 5)
+}
+
+
+function getMostPopularBooks(books) {
+  return books.map(book => {
+    return {name: book.title, count: book.borrows.length}
+   }).sort((bookA, bookB) => (bookA.count < bookB.count ? 1 : -1)).slice(0, 5)
+}
+
+
+function getMostPopularAuthors(books,authors) {
+  const topAuthors = authors.map(author => ({
+  ...author,
+  bookCount: books.filter(book => book.authorId === author.id).length,
+  borrowCount: books.filter(book => book.authorId === author.id).reduce((acc, cur) => acc + cur.borrows.length, 0)
+  })).sort((alpha, beta) => beta.borrowCount - alpha.borrowCount);
+  topAuthors.length = 5;
+  return topAuthors.map(ta => {
+  return {count: ta.borrowCount, name: ta.name.first + " " + ta.name.last};
+  })
+  }
+
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
